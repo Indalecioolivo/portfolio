8292bc6cb2e15db58034cc90082fb1f8f7ce3e09
Indalecio Olivo <indalecioolivo@gmail.com>
2024-12-05 10:06:42 -0300

    style: modificado cor do header em modo dark e light

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,22 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="header-container">
-      <img src={Triangles} alt="" />
+      {/* <img src={Triangles} alt="" /> */}
+      <svg
+        width="3.8rem"
+        height="2rem"
+        viewBox="0 0 61 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          id="Combined Shape"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M31.3469 26.1224L15.6735 0L0 26.1224H31.3469ZM60.0816 5.87755L44.4082 32L28.7347 5.87755H60.0816Z"
+          fill="rgb(234, 234, 235)"
+        />
+      </svg>
       <nav>
         <Link to="/" className="nav-option">
           Home
